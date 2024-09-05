57d16e989d2af806ab06a64ea77ae59f78ba3b8f
mateus52 <mateusdossantos52@gmail.com>
2024-09-05 09:56:52 -0300

    Initial commit

---
+++ src/main/java/com/MoneyTrackr/MoneyTrackr/MoneyTrackrApplication.java
@@ -0,0 +1,13 @@
+package com.MoneyTrackr.MoneyTrackr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyTrackrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyTrackrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MoneyTrackr/MoneyTrackr/MoneyTrackrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MoneyTrackr.MoneyTrackr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyTrackrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
